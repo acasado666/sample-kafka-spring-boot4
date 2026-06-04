23c927f8ff460936c4b655858815e27ca75e7eda
Antonio Casado <tonicasfer23@gmail.com>
2026-06-04 20:54:21 +0200

    Constants class

--- order-events-producer/src/main/java/com/kodebytes/acasado/config/AppConstants.java
+++ order-events-producer/src/main/java/com/kodebytes/acasado/config/AppConstants.java
@@ -2,12 +2,11 @@ package com.kodebytes.acasado.config;
 
 public final class AppConstants {
 
-    private AppConstants() {
-        // Utility class
-    }
+    // Utility class make sure it cannot be instantiated
+    private AppConstants() { }
 
     public static final String API_BASE_PATH = "/api/orderevent";
-    public static final String DEFAULT_LIBRARY_EVENTS_TOPIC = "order-events";
+    public static final String DEFAULT_LIBRARY_EVENTS_TOPIC = "library-events";
     public static final String DEFAULT_PRODUCER_ACKS = "all";
 }
 
