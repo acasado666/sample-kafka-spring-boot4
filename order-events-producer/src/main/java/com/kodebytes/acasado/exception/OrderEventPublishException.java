133817adfdbfa3c8e423ccea2e3bf035c6b99831
Antonio Casado <tonicasfer23@gmail.com>
2026-06-06 15:31:34 +0200

    Rename OrderEventPublishException

--- order-events-producer/src/main/java/com/kodebytes/acasado/exception/OrderEventPublishException.java
+++ order-events-producer/src/main/java/com/kodebytes/acasado/exception/OrderEventPublishException.java
@@ -3,9 +3,9 @@ package com.kodebytes.acasado.exception;
 /**
  * Thrown when the Kafka producer fails to publish a {@code OrderEvent}.
  */
-public class OrderEventException extends RuntimeException {
+public class OrderEventPublishException extends RuntimeException {
 
-    public OrderEventException(String message, Throwable cause) {
+    public OrderEventPublishException(String message, Throwable cause) {
         super(message, cause);
     }
 }
