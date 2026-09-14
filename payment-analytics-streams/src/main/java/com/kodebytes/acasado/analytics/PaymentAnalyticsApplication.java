444cf216edbc8281b4eba618d5ee597a109cb190
Antonio Casado <tonicasfer23@gmail.com>
2026-09-14 14:45:11 +0200

    fix: enable managed Kafka Streams infrastructure

--- payment-analytics-streams/src/main/java/com/kodebytes/acasado/analytics/PaymentAnalyticsApplication.java
+++ payment-analytics-streams/src/main/java/com/kodebytes/acasado/analytics/PaymentAnalyticsApplication.java
@@ -2,7 +2,8 @@ package com.kodebytes.acasado.analytics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-@SpringBootApplication @ConfigurationPropertiesScan
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+@SpringBootApplication @ConfigurationPropertiesScan @EnableKafkaStreams
 public class PaymentAnalyticsApplication {
  public static void main(String[] args){SpringApplication.run(PaymentAnalyticsApplication.class,args);}
 }
